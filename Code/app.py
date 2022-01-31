4b7f7295345fed2b96480fae12b59d1f3f964b2e
Dani Roxberry <dani@bitoriented.com>
2022-01-31 13:52:50 -0800

    [add] before_first_request optimization

--- Code/app.py
+++ Code/app.py
@@ -1,7 +1,14 @@
+"""Main script, uses other modules to generate sentences."""
 from flask import Flask
 
 
 app = Flask(__name__)
+
+
+@app.before_first_request
+def before_first_request():
+    """Runs only once at Flask startup"""
+    # TODO: Initialize your histogram, hash table, or markov chain here.
 
 
 @app.route("/")
